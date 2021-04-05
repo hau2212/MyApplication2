6bb0e1a5f33c50b30d7892c9422971aa32c3b70e
nguyen thanh hau <mr.hau221222@gmail.com>
2021-04-05 19:14:10 +0700

    hello 12

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,22 +1,21 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
 
-
-    EditText mht1,ht2;
+    EditText mht1, ht2;
     TextView htkq;
-    Button bt1,bt2,bt3,bt4;
-
+    Button bt1, bt2, bt3, bt4, kq12;
+    String chua1, chua2;
 
 
     @Override
@@ -29,15 +28,35 @@ public class MainActivity extends AppCompatActivity {
         mht1 = findViewById(R.id.so1);
         ht2 = findViewById(R.id.so2);
         htkq = findViewById(R.id.kq);
+        kq12 = findViewById(R.id.kq2);
         bt1 = findViewById(R.id.btcong);
         bt2 = findViewById(R.id.bttru);
-        bt3 =findViewById(R.id.btnhan);
-        bt4 =findViewById(R.id.btchia);
+        bt3 = findViewById(R.id.btnhan);
+        bt4 = findViewById(R.id.btchia);
 
         htkq.setText("10+10=22");
         //toast
         //Toast.makeText(this,"thong bao ",Toast.LENGTH_LONG).show();
 
-        Log.d("KKK",htkq.getText().toString());
+
+
+
+        //click
+        kq12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "nhan roi", Toast.LENGTH_LONG).show();
+                chua1 = mht1.getText().toString();
+                chua2 = ht2.getText().toString();
+
+                if (chua1.length() <= 0 || chua2.length() <= 0){
+                    Toast.makeText(MainActivity.this, "chua nhap", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+
+            }
+        });
+
     }
 }
